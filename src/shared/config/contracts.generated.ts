897cec0613284d46734a047d8a11ebd1b8db9cd5
Andrei Lopes <andreipsva@gmail.com>
2026-07-21 21:50:46 +0100

    update rules

--- src/shared/config/contracts.generated.ts
+++ src/shared/config/contracts.generated.ts
@@ -1,5 +1,5 @@
 /** Written/updated by root stack sync after local deploy. */
 export const generatedContracts = {
-  identityRegistryAddress: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-  tokenAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+  identityRegistryAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  tokenAddress: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 } as const;
